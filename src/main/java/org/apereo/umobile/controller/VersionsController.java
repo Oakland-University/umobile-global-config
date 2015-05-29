d3120ea8f500644a29552aa5ae43421d242eb3a2
KaJuan Johnson <kdjohnson23@oakland.edu>
2015-05-29 10:45:41 -0400

    Add variables with meaning

--- src/main/java/org/apereo/umobile/controller/VersionsController.java
+++ src/main/java/org/apereo/umobile/controller/VersionsController.java
@@ -9,11 +9,7 @@ import java.io.File;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import org.apereo.umobile.service.IPropertiesService;
-
 import org.springframework.stereotype.Controller;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -25,38 +21,28 @@ public class VersionsController {
     private final ClassLoader CLASSLOADER = getClass().getClassLoader();
     private final String PRE_FILE_LOCATION = "mobile"; 
 
-	@Autowired
-	private IPropertiesService propertiesService;
-
 	@RequestMapping(value="/versions", method = RequestMethod.GET, produces = "application/json")
 	public @ResponseBody Map<String, List> getConfig (HttpServletRequest request, HttpServletResponse response) {
 		
-   Map<String, List> tester = new LinkedHashMap<String, List>();
-   List<String> list = new ArrayList<String>();
+    Map<String, List> map = new LinkedHashMap<String, List>();
 
-    File dir = new File(CLASSLOADER.getResource(PRE_FILE_LOCATION).getFile());
-    File[] files = dir.listFiles();
+    File directories = new File(CLASSLOADER.getResource(PRE_FILE_LOCATION).getFile());
+    File[] directoryList = directories.listFiles();
 
-    for(File file: files){
-            if(file.isDirectory()){
-                File dir2 = new File(CLASSLOADER.getResource(PRE_FILE_LOCATION + "/" + file.getName()).getFile());
-                File[] files2 = dir2.listFiles();
-                for(File file2: files2){
-                        if(file2.isDirectory()){
-                                list.add(file2.getName());  
-                            }
-                            else{
-                                System.out.println("Hello");   
-                             }
-                    }
-                tester.put(file.getName(), list);
-               }
-            else{
-                System.out.println("No"); 
-              }
-           list = new ArrayList<String>();
+    for(File folder: directoryList){
+      List<String> list = new ArrayList<String>();
+      if(folder.isDirectory()){
+        File subDirectories = new File(CLASSLOADER.getResource(PRE_FILE_LOCATION + "/" + folder.getName()).getFile());
+        File[] subDirectoryList = subDirectories.listFiles();
+        for(File subFolder: subDirectoryList){
+          if(subFolder.isDirectory()){
+            list.add(subFolder.getName());  
+          }
         }
-        return tester; 
-	}
+        map.put(folder.getName(), list);
+      }
+    }
+    return map; 
+  }
 
 }
